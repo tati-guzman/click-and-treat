e53ce03d2987d91fdea10a569d455307b8f556e6
Tati Guzman <tati.guzman96@gmail.com>
2024-10-17 15:04:20 -0700

    Adds pseudocode for all planned routes

--- server/server.js
+++ server/server.js
@@ -11,7 +11,40 @@ const PORT = process.env.PORT || 8030;
 app.use(cors());
 app.use(express.json());
 
-//All Routes: Need to add more pseudocode
+//All Planned Routes
+
+//POST Route to create a new user
+//'/users'
+
+//May Delete: GET Route to check username and password
+//'/users'
+
+//GET Route to pull pet information for a particular user
+//'/pets/:user_id'
+
+//GET Route to pull all session data for each training plan
+//'/sessions/history/:plan_id'
+
+//POST Route to submit information for a new session
+//'/sessions'
+
+//PUT Route to edit session information (stretch goal)
+//'/sessions/:session_id'
+
+//PUT Route to edit user information
+//'/users/:user_id'
+
+//POST Route to create a new pet
+//'/pets'
+
+//PUT Route to update pet information
+//'/pets/:pet_id
+
+//DELETE Route to delete a pet
+//'/pets/:pet_id
+
+//POST Route to create a new association between two members (stretch goal)
+//'/family'
 
 //Print PORT location when active PORT is detected (server is running properly)
 app.listen(PORT, () => {
