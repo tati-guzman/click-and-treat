ff2fdce445a739bcd3406529a96e3f57e7df6278
Tati Guzman <tati.guzman96@gmail.com>
2024-10-18 13:12:41 -0700

    Adds more detailed comments to Nav Bar and associated components to prep for skeleton demo

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -8,7 +8,11 @@ const About = () => {
     return (
         <div>
             <h1>About Page</h1>
+
+            {/* Add in actual information as well as relevant pictures! */}
             <p>Blurb about the pages!</p>
+            <p>Info about Techtonica, Tati, and Daisy!</p>
+            
         </div>
     )
 }
--- client/src/components/Examples.jsx
+++ client/src/components/Examples.jsx
@@ -5,7 +5,13 @@ const Examples = () => {
 
     return (
         <div>
-            <h1>Examples</h1>
+            <h1>Example Training Plans</h1>
+
+            <h3>Currently Offered Plans</h3>
+            <p>Information about Middle and how to complete it goes here</p>
+            
+            <h3>Other Plans</h3>
+            <p>Details about plans such as Touch and Vacuum Monster go here!</p>
         </div>
     )
 }
