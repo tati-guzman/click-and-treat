b6c36208291cda6db326d865c181ba058e1fc3cb
Tati Guzman <tati.guzman96@gmail.com>
2024-10-18 11:47:39 -0700

    Creates context file to use in useContext within main App

---
+++ client/src/ComponentContext.js
@@ -0,0 +1,8 @@
+//Regular importing of React functionality
+import React from 'react';
+
+//Create context for which component we should be seeing
+const ComponentContext = React.createContext();
+
+//Export for use throughout components
+export default ComponentContext;
