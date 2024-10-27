dfe6b6cf735a671239d995d739b87162613d052e
Tati Guzman <tati.guzman96@gmail.com>
2024-10-27 14:03:29 -0700

    Now commit appropriate component with the following changes - Transforms buttons into links to route to appropriate pages per React Router, and creates temporary loggedUser state

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,35 +1,33 @@
 //Import necessary functionalities
-import React, { useContext } from 'react';
-import UserComponentContext from '../UserComponentContext.js';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 //Import log in component to hang out in the upper right hand corner of the page
 import LogIn from './LogIn.jsx';
 
-const NavBar = ({ }) => {
+const NavBar = () => {
 
-    //Import display and user settings to use in display control and user updates
-    const { display, user } = useContext(UserComponentContext);
-    const [component, setComponent] = display;
-    const [loggedUser, setLoggedUser] = user;
+    //Temporary state to hold logged user - update with useContext and OAuth info
+    const [loggedUser, setLoggedUser] = useState(null);
 
-    //Logout function to remove logged user and return to homepage
+    //Logout function to remove logged user
     const logout = () => {
         setLoggedUser(null);
-        setComponent('homepage');
     }
 
     return (
         <nav>
             {/* Replace header with logo in the future */}
-            <h1 onClick={() => loggedUser ? setComponent('dashboard') : setComponent('homepage')}>Click and Treat</h1>
+            {/* <h1 onClick={() => loggedUser ? setComponent('dashboard') : setComponent('homepage')}>Click and Treat</h1> */}
+            {/* Need to create function to link to homepage or dashboard based on loggedUser */}
+            <Link to="/homepage">Click and Treat</Link>
             
-            {/* Potentially just change into links rather than buttons */}
-            <button onClick={() => setComponent('about')}>About</button>
-            <button onClick={() => setComponent('examples')}>Example Training Plans</button>
+            <Link to="/about">About</Link>
+            <Link to="/examples">Example Training Plans</Link>
 
             {loggedUser 
-            ? <button onClick={logout}>Log Out</button>
-            : <LogIn />}
+            ? <Link to="/homepage"><button onClick={logout}>Log Out</button></Link>
+            : <LogIn loggedUser={loggedUser} setLoggedUser={setLoggedUser} />}
         </nav>
     )
 }
