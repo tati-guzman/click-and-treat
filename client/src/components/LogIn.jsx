7216b96c28c2f34de82c18a3716f1f06009e15c0
Tati Guzman <tati.guzman96@gmail.com>
2024-10-27 14:00:19 -0700

    Revert "Transforms buttons into links to route to appropriate pages per React Router, and creates temporary loggedUser state"
    This reverts commit 25e9715df7f880f83a0b8eef5e2285c99c76ad78.

--- client/src/components/LogIn.jsx
+++ client/src/components/LogIn.jsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react';
 import LogInModal from './LogInModal.jsx';
 import NewAccountModal from './NewAccountModal.jsx';
 
-const LogIn = ({ loggedUser, setLoggedUser }) => {
+const LogIn = () => {
     
     //State to hold display status of both modals
     const [logInModal, setLogInModal] = useState(false);
@@ -17,7 +17,7 @@ const LogIn = ({ loggedUser, setLoggedUser }) => {
             <button onClick={()=> setLogInModal(true)}>Log In</button>
             <button onClick={()=> setNewAccountModal(true)}>Create an Account</button>
 
-            {logInModal && <LogInModal isOpen={logInModal} onClose={() => setLogInModal(false)} loggedUser={loggedUser} setLoggedUser={setLoggedUser}/>}
+            {logInModal && <LogInModal isOpen={logInModal} onClose={() => setLogInModal(false)}/>}
             {newAccountModal && <NewAccountModal isOpen={newAccountModal} onClose={() => setNewAccountModal(false)}/>}
             
         </div>
