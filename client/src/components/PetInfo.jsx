c42adf1b0b1a8118d049c3fdd59b844179cc443e
Tati Guzman <tati.guzman96@gmail.com>
2024-10-28 12:20:56 -0700

    Corrects loadPetInfo function to pull all pet data upon render and display each pet correctly with a button to select a pet for training

--- client/src/components/PetInfo.jsx
+++ client/src/components/PetInfo.jsx
@@ -1,47 +1,69 @@
 //Import necessary functionalities
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { UserStatus } from '../context/UserContext';
 
-const PetInfo = () => {
+const PetInfo = ({ setSelectedPet }) => {
+
+    //Use useNavigate hook from React Router to establish functionality in buttons below
+    const navigate = useNavigate();
 
     //Import user info for use in data pull
     const { loggedUser, setLoggedUser } = UserStatus();
-    console.log("from pet info", loggedUser);
+    
+    //State to hold pet information
+    const [petInfo, setPetInfo] = useState(null);
 
-   //State to hold pet information
-   const [petInfo, setPetInfo] = useState([]);
+    //Function to load pet information from database
+    const loadPetInfo = async () => {
+        const userId = loggedUser.userId;
+        
+        //Send query with userId as parameter to pull all pets from this user (as primary or secondary user)
+        const response = await fetch(`/api/pets/${userId}`);
 
-   //Function to load pet information from database
-   const loadPetInfo = async () => {
-    debugger;
-    console.log("Logged User");
-    console.log(loggedUser);
+        //Save response in a variable to access for display
+        const petData = await response.json();
 
-    // const userNumber = user[0]
-    const response = await fetch(`/api/pets/1`);
-
-    const petData = await response.json();
-
-    if (!petData) {
-        console.log("error retrieving pet data");
-    } else {
-        setPetInfo(petData);
-        console.log(petInfo);
-    }
-
+        if (!petData[0].pets) {
+            setPetInfo(null);
+        } else {
+            //Remove the first element of the array (pet status)
+            petData.shift();
+            //Store the rest of the data (each pet's info) in the petData state
+            setPetInfo(petData);
+        }
    }
 
     //Use Effect to download pet info when the component loads
-    // useEffect(() => {
-    //     loadPetInfo();
-    // }, []);
+    useEffect(() => {
+        loadPetInfo();
+    }, []);
+
+    //Function to format pet information in display
+    const formatPetDisplay = () => {
+        return petInfo.map((pet, index) => (
+            <div key={index}>
+                <h4>Pet Name: {pet.name}</h4>
+                <p>Species: {pet.species}</p>
+                <button onClick={() => selectPet(index)}>View Training Plans</button>
+            </div>
+        ))        
+    }
+
+    //Function to set selected pet state for use in UserTrainingPlans component
+    const selectPet = (index) => {
+        const petId = petInfo[index].pet_id;
+        setSelectedPet({petId: petId});
+    }
 
     return (
         <div>
-            <h1>Pet Info Component</h1>
+            <h1>Your Pets</h1>
+            
+            {petInfo ? formatPetDisplay() : <p>No pets associated with this account</p>}
 
-            <Link to="/account"><button>Add More Pets (send to account info component)</button></Link>
+            <button onClick={()=>{navigate("/account")}}>Add More Pets +</button>
+        
         </div>
     )
 }
