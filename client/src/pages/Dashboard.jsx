cea7b9bdd47156f812f0cb21b3bf6ef0ea23b37a
Tati Guzman <tati.guzman96@gmail.com>
2024-10-27 19:18:04 -0700

    Imports React Router layout to include nav bar at the top of the display

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,33 @@
+//Import necessary functionalities
+import React from 'react';
+import MainLayout from '../layout/MainLayout';
+import { UserStatus } from '../context/UserContext';
+
+//Import components to be displayed
+import UserTrainingPlans from '../components/UserTrainingPlans';
+import MotivationalQuotes from '../components/MotivationalQuotes';
+import PetInfo from '../components/PetInfo';
+
+const Dashboard = () => {
+    
+    console.log("Woohoo! You logged in to a user dashboard!");
+    
+   //Import user settings to use in displaying name
+   const { loggedUser, setLoggedUser } = UserStatus();
+
+    return (
+        <MainLayout>
+            <div>
+                <h1>Dashboard Component (Greetings, Name!)</h1>
+
+                <PetInfo />
+    
+                <UserTrainingPlans />
+
+                <MotivationalQuotes />
+            </div>
+        </MainLayout>
+    )
+}
+
+export default Dashboard;
