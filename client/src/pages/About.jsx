9242ec014bf4c9aa8c6a83fa393202e4e9613543
Tati Guzman <tati.guzman96@gmail.com>
2024-11-11 21:23:16 -0800

    Adds pictures to About Page and formats information to fit evenly around the pictures

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,24 +1,27 @@
 //Import necessary functionalities
 import React from 'react';
 import MainLayout from '../layout/MainLayout';
+import daisy from '../assets/derpy_daisy.png';
+import tati from '../assets/tati.png';
+import cats from '../assets/hugging_cats.png';
 
 const About = () => {
 
     return (
         <MainLayout>
-            <div className="general">
-                <div>
-                    <h1>Why Click and Treat?</h1>
-                    <p>Click and Treat began as a final project for <a href="https://techtonica.org/full-time-program/" target="_blank" rel="noreferrer noopener">Techtonica</a>'s full-time coding program. Click and Treat's founder, <a href="https://www.linkedin.com/in/tati-guzman" target="_blank" rel="noreferrer noopener">Tati Guzman</a>, wanted to build a tool that she herself could use in her every day life.</p>
-                </div>
-
-                <div>
-                    <h1>Okay, but what am I clicking?</h1>
-                    <p>At Click and Treat, we are huge proponents of <a href="https://www.petmd.com/dog/behavior/how-to-train-a-dog-with-positive-reinforcement" target="_blank" rel="noreferrer noopener">positive reinforcement training</a>. Training does not ever require fear or punishment. Instead, our pets should be rewarded for the behaviors we want to see and given the structure and patience to meet the goals we set out for them. Markers help communicate exactly what was done to earn the treat/reinforcement, which helps our pets repeat that behavior! You can use a verbal prompt, <a href="https://www.chewy.com/sungrow-clickers-wrist-band-dog-cat/dp/241963?utm_source=google-product&utm_medium=cpc&utm_campaign=20027453190&utm_content=&gad_source=1&gclid=Cj0KCQjw1Yy5BhD-ARIsAI0RbXanQDkH7sRLq4VmjeO7Rnly8DvafmwGsIVuhFdIOUJD91sjPmrJwAcaArLHEALw_wcB" target="_blank" rel="noreferrer noopener">clicker</a>, or even just a clicky pen. When you see the behavior you are cueing, simply click and treat!</p>
-                </div>
+            <div className="about">
+                <h1>Why Click and Treat?</h1>
+                <p>Click and Treat began as a final project for <a href="https://techtonica.org/full-time-program/" target="_blank" rel="noreferrer noopener">Techtonica</a>'s full-time coding program. Click and Treat's founder, <a href="https://www.linkedin.com/in/tati-guzman" target="_blank" rel="noreferrer noopener">Tati Guzman</a>, wanted to build a tool that she herself could use in her every day life.</p>
+           
+                <img className="about-pictures" src={cats} alt='Two tabby kittens are asleep in a cat tree. They are hugging side by side.'/>
+                <img className="about-pictures" src={tati} alt='A young Latina woman is sitting on a gray bean bag with a tan pitbull on her lap. She is smiling toward the camera while the dog looks at the camera with a closed mouth.'/>
+                <img className="about-pictures" src={daisy} alt='A tan pitbull is close to the camera with her tongue slightly out. Behind her, the sky is bright blue with no clouds. '/>
+           
+                <h1>Okay, but what am I clicking?</h1>
+                <p>At Click and Treat, we are huge proponents of <a href="https://www.petmd.com/dog/behavior/how-to-train-a-dog-with-positive-reinforcement" target="_blank" rel="noreferrer noopener">positive reinforcement training</a>. Training does not ever require fear or punishment. Instead, our pets should be rewarded for the behaviors we want to see and given the structure and patience to meet the goals we set out for them. Markers help communicate exactly what was done to earn the treat/reinforcement, which helps our pets repeat that behavior! You can use a verbal prompt, <a href="https://www.chewy.com/sungrow-clickers-wrist-band-dog-cat/dp/241963?utm_source=google-product&utm_medium=cpc&utm_campaign=20027453190&utm_content=&gad_source=1&gclid=Cj0KCQjw1Yy5BhD-ARIsAI0RbXanQDkH7sRLq4VmjeO7Rnly8DvafmwGsIVuhFdIOUJD91sjPmrJwAcaArLHEALw_wcB" target="_blank" rel="noreferrer noopener">clicker</a>, or even just a clicky pen. When you see the behavior you are cueing, simply click and treat!</p>
+           
 
                 {/* Future Sections should include: blurb about Tati, some fun facts about Techtonica, and pictures of Daisy, Luke, and Han! */}
-                
             </div>
         </MainLayout>
     )
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -22,11 +22,11 @@ const HomePage = () => {
                             <p>While we may not recommend training your neighborhood raccoon, we know that pet training is for more than just dogs. Add any of your pets to track their individual progress.</p>
                         </div>
 
-                        <img className="homepage-picture" src={park} alt='stuff'/>
+                        <img className="homepage-picture" src={park} alt='A tan pitbull mix is laying on grass and looking up at the camera with an open mouth conveying happiness'/>
                     </div>
                     
                     <div className="row">
-                        <img className="homepage-picture" src={dogs} alt='stuff'/>
+                        <img className="homepage-picture" src={dogs} alt='Three dogs are sitting next to each other on concrete. They are dressed as a lion, pirate, and rock star.'/>
                         
                         <div className="homepage-column">
                             <h3>TAKE IT STEP BY STEP</h3>
@@ -40,11 +40,11 @@ const HomePage = () => {
                             <p>Keeping track of feeding and walking schedules is enough. No need to also try to remember your pet's personal record on a certain trick or the treat that's gotten the most wags. Jot down all of your notes for each session and they'll be waiting for you next time.</p>
                         </div>
     
-                        <img className="homepage-picture" src={kitten} alt='stuff'/>
+                        <img className="homepage-picture" src={kitten} alt='A white and gray tabby is sleeping with a sliver of his tongue sticking out.'/>
                     </div>
 
                     <div className="row">
-                        <img className="homepage-picture" src={fetch} alt='stuff'/>
+                        <img className="homepage-picture" src={fetch} alt='A tan pitbull mix is running towards the camera on concrete with a bright orange ball in her mouth.'/>
     
                         <div className="homepage-column">
                             <h3>ALL ABOUT THE JOURNEY</h3>
@@ -58,7 +58,7 @@ const HomePage = () => {
                             <p>Have you ever cued a pet to do something only for your partner to say they taught them the same skill with a different name? We know pets often have multiple humans in their lives that are invested in their training. Soon, you'll be able to connect your account with a friend or family member to access all of your pet's training data, not just your own.</p>
                         </div>
                         
-                        <img className="homepage-picture" src={cats} alt='stuff'/>
+                        <img className="homepage-picture" src={cats} alt='Two tabby cats are sitting next to each other with a button in between them. They are looking up at the camera.'/>
                     </div>
                     
                 </div>
