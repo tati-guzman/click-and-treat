4730c126a4598e6e5600c7d785c0ea77b70c99c1
Tati Guzman <tati.guzman96@gmail.com>
2024-11-11 20:25:09 -0800

    Adds and styles images to go alongside homepage blurbs

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,6 +1,11 @@
 //Import necessary functionalities
 import React from 'react';
 import MainLayout from '../layout/MainLayout';
+import kitten from '../assets/sleeping_kitten.jpg'
+import cats from '../assets/cats_together.png';
+import dogs from '../assets/dogs_costume.png';
+import fetch from '../assets/fetch.png';
+import park from '../assets/dog_park.png';
 
 const HomePage = () => {
 
@@ -12,48 +17,48 @@ const HomePage = () => {
 
                 <div className="homepage">
                     <div className="row">
-                        <div className="homepage-columnA">
+                        <div className="homepage-column">
                             <h3>NO BREED RESTRICTIONS HERE!</h3>
                             <p>While we may not recommend training your neighborhood raccoon, we know that pet training is for more than just dogs. Add any of your pets to track their individual progress.</p>
                         </div>
 
-                        <img className="homepage-picture" src='assets' alt='stuff'/>
+                        <img className="homepage-picture" src={park} alt='stuff'/>
                     </div>
                     
                     <div className="row">
-                        <img className="homepage-picture" src='assets' alt='stuff'/>
+                        <img className="homepage-picture" src={dogs} alt='stuff'/>
                         
-                        <div className="homepage-columnB">
+                        <div className="homepage-column">
                             <h3>TAKE IT STEP BY STEP</h3>
                             <p>Our training plans break down skills into manageable stages to help set you and your pet up for success! Choose from our library of training plans or create your own (coming soon).</p>
                         </div>
                     </div>
                     
                     <div className="row">
-                        <div className="homepage-columnA">
+                        <div className="homepage-column">
                             <h3>REDUCE YOUR MENTAL LOAD</h3>
                             <p>Keeping track of feeding and walking schedules is enough. No need to also try to remember your pet's personal record on a certain trick or the treat that's gotten the most wags. Jot down all of your notes for each session and they'll be waiting for you next time.</p>
                         </div>
     
-                        <img className="homepage-picture" src='assets' alt='stuff'/>
+                        <img className="homepage-picture" src={kitten} alt='stuff'/>
                     </div>
 
                     <div className="row">
-                        <img className="homepage-picture" src='assets' alt='stuff'/>
+                        <img className="homepage-picture" src={fetch} alt='stuff'/>
     
-                        <div className="homepage-columnB">
+                        <div className="homepage-column">
                             <h3>ALL ABOUT THE JOURNEY</h3>
                             <p>Celebrate your pet's progress, growth, and sometimes roller coaster of a training journey by viewing your sessions at a glance. While you can access each session's details individually, sometimes it's the big picture that can really show us how far we've come.</p>
                         </div>
                     </div>
                     
                     <div className="row">
-                        <div className="homepage-columnA">
+                        <div className="homepage-column">
                             <h3>COMING SOON: CONNECT WITH YOUR SUPPORT NETWORK</h3>
                             <p>Have you ever cued a pet to do something only for your partner to say they taught them the same skill with a different name? We know pets often have multiple humans in their lives that are invested in their training. Soon, you'll be able to connect your account with a friend or family member to access all of your pet's training data, not just your own.</p>
                         </div>
                         
-                        <img className="homepage-picture" src='assets' alt='stuff'/>
+                        <img className="homepage-picture" src={cats} alt='stuff'/>
                     </div>
                     
                 </div>
