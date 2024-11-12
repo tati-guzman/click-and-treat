db1e18133cdb324ad75a738496c0893d565df44e
Tati Guzman <tati.guzman96@gmail.com>
2024-11-11 21:56:15 -0800

    Removes empty forms from Account Information page and replaces them with a Coming Soon banner instead

--- client/src/pages/AccountInformation.jsx
+++ client/src/pages/AccountInformation.jsx
@@ -1,8 +1,8 @@
 //Import necessary functionalities and components
-import React, { useContext } from 'react';
-import PetInfoForm from '../components/PetInfoForm';
-import ConnectionForm from '../components/ConnectionForm';
-import UserInfoForm from '../components/UserInfoForm';
+import React from 'react';
+// import PetInfoForm from '../components/PetInfoForm';
+// import ConnectionForm from '../components/ConnectionForm';
+// import UserInfoForm from '../components/UserInfoForm';
 import MainLayout from '../layout/MainLayout';
 
 const AccountInformation = () => {
@@ -10,14 +10,15 @@ const AccountInformation = () => {
     return (
         <MainLayout>
             <div className="general">
-                <h1>Account Information Page</h1>
-                <p>Needs to include account info form, pet info form, and stretch goal of connecting to other users (also a form)</p>
+                <h1>*** Account Information Page Coming Soon ***</h1>
+                {/* <h1>Account Information Page</h1>
+                <p>Needs to include account info form, pet info form, and stretch goal of connecting to other users (also a form)</p> */}
 
                 {/* This just requires formatting so the three forms exist but functionality is within each form */}
 
-                <PetInfoForm />
+                {/* <PetInfoForm />
                 <UserInfoForm />
-                <ConnectionForm />
+                <ConnectionForm /> */}
 
             </div>
         </MainLayout>
